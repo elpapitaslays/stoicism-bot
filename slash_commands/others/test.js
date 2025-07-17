3af40a79f9291d44451ef69f42b45a30e49d34f0
Felipe Werner <folomusic594@gmail.com>
2025-07-17 20:32:05 -0300

    bro idek what the fuck im doing

---
+++ slash_commands/others/test.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("test")
+    .setDescription("usa esto para testear el bot"),
+    
+    async execute(interaction){
+        await interaction.reply(`¡Hola ${interaction.user.username}, estoy funcionando! ¡Gracias por usarme!`);
+    }
+}
